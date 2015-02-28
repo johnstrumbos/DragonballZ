02d2d4a54ed912321987e88146bf1b14e876f2ef
Dashon Howard <dhoward@pathf.com>
2015-02-28 02:00:47 -0800

    stub some view controllers

---
+++ PanelChat/NotificationsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationsViewController.swift
+//  PanelChat
+//
+//  Created by Dashon Howard on 2/28/15.
+//  Copyright (c) 2015 PanelChat. All rights reserved.
+//
+
+import Foundation
---
+++ PanelChat/PanelDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PanelDetailViewController.swift
+//  PanelChat
+//
+//  Created by Dashon Howard on 2/28/15.
+//  Copyright (c) 2015 PanelChat. All rights reserved.
+//
+
+import Foundation
